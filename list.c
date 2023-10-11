c2524d933abaa0b4fae98130d9ae9949d3fc617b
zsantos26 <26zeth@gmail.com>
2023-10-11 10:49:10 -0700

    added data structures in list.h, heavily inspired my course material (CMPT300);

---
+++ list.c
@@ -0,0 +1,56 @@
+/*
+ * list.c
+ *
+ * Description: Implementing functions declared in list.h Doubly linked list data structure, function declarations, etc. 
+ * 
+ * Author: Zeth Santos
+ * Date: Oct 10 2023
+*/
+
+#include "list.h"
+#include <assert.h>
+#include <stdio.h>
+
+// Static arrays for nodes and heads
+static Node nodes[LIST_MAX_NUM_NODES];
+static List lists[LIST_MAX_NUM_HEADS];
+
+// Free lists
+static int freeNodes[LIST_MAX_NUM_NODES];
+static int freeNodeTop = -1;  // Top of the stack for free nodes
+
+static int freeHeads[LIST_MAX_NUM_HEADS];
+static int freeHeadTop = -1;  // Top of the stack for free lists
+
+// Makes a new, empty list, and returns its reference on success. 
+// Returns a NULL pointer on failure.
+List* List_create() {
+    static bool initialized = false;
+    if (!initialized) {
+        // Populate free lists
+        for (int i = 0; i < LIST_MAX_NUM_NODES; i++) {
+            freeNodes[++freeNodeTop] = i;
+        }
+        for (int i = 0; i < LIST_MAX_NUM_HEADS; i++) {
+            freeHeads[++freeHeadTop] = i;
+        }
+        initialized = true;
+    }
+
+    if (freeHeadTop == -1) {
+        return NULL;  // No free lists available
+    }
+    int newListIndex = freeHeads[freeHeadTop--];
+    List* newList = &lists[newListIndex];
+    newList->start = -1;
+    newList->end = -1;
+    newList->curr = -1;
+    newList->count = 0;
+    return newList;
+}
+
+// Returns the number of items in pList.
+int List_count(List* pList) {
+    assert(pList != NULL);
+    return pList->count;
+}
--- list.h
+++ list.h
@@ -6,3 +6,45 @@
  * Author: Zeth Santos
  * Date: Oct 9 2023
 */
+
+
+// List data type
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdbool.h>
+
+#define LIST_SUCCESS 0
+#define LIST_FAIL -1
+
+typedef struct Node_s Node;
+struct Node_s {
+    void* data;
+    int next;  // Index to the next node
+    int prev;  // Index to the previous node
+};
+
+enum ListOutOfBounds {
+    LIST_OOB_START,
+    LIST_OOB_END
+};
+typedef struct List_s List;
+struct List_s {
+    int start;  // Index to the first node
+    int end;    // Index to the last node
+    int curr;   // Index to the current node
+    int count;  // Number of elements in the list
+};
+// Max number of unique lists
+#define LIST_MAX_NUM_HEADS 10
+
+// Max number of nodes over all lists
+#define LIST_MAX_NUM_NODES 100
+
+// Create new list
+List* List_create();
+
+// Count items in list
+int List_count(List* pList);
+
+
+#endif
